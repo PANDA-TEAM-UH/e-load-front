ee769c5b87e89ab25115e94cecab02a8cf7da272
fpascuales <fpascuales@gmail.com>
2023-06-03 13:43:03 +0200

    Create comments & spots actions / reducers

--- src/redux/spots/spots.actions.js
+++ src/redux/spots/spots.actions.js
@@ -1,3 +1,89 @@
+import { API } from "../../shared/Api";
 import store from "../store";
 
 const { dispatch } = store;
+
+const createSpot = async (dataSpot, station) => {
+    try {
+        dispatch({type: "LOADING"});
+        const formData = new FormData();
+        formData.append("power", dataSpot.power);
+        formData.append("type", dataSpot.type);
+        formData.append("rate", dataSpot.rate);
+        formData.append("state", dataSpot.state);
+        formData.append("station", station);
+        const result = await API.post("spots", formData);
+        dispatch({
+            type: "CREATE_SPOT",
+            payload: result.data
+        });
+    } catch (error) {
+        dispatch({ type: "ERROR", payload: error.response.data });
+    }
+}
+const getAllSpots = async () => {
+    try {
+        dispatch({type: "LOADING"});
+        const result = await API.get("spots");
+        dispatch({
+            type: "GET_SPOTS",
+            payload: result.data
+        });
+    } catch (error) {
+        dispatch({ type: "ERROR", payload: error.response.data });
+    }
+}
+const getSpotsByStation = async (stationId) => {
+    try {
+        dispatch({type: "LOADING"});
+        const result = await API.get(`stations/${stationId}`);
+        dispatch({
+            type: "STATION_SPOTS",
+            payload: result.data.spots
+        });
+    } catch (error) {
+        dispatch({ type: "ERROR", payload: error.response.data });
+    }
+}
+const updateSpot = async (spotId, spotToUpdate) => {
+    try {
+        dispatch({type: "LOADING"});
+        const formData = new FormData();
+        formData.append("power", spotToUpdate.power);
+        formData.append("type", spotToUpdate.type);
+        formData.append("rate", spotToUpdate.rate);
+        formData.append("state", spotToUpdate.state);
+        const result = await API.put(`spots/${spotId}, formData`);
+        dispatch({
+            type: "UPDATE_SPOT",
+            payload: result.data
+        });
+    } catch (error) {
+        dispatch({ type: "ERROR", payload: error.response.data });
+    }
+}
+const deleteSpot = async (spotId) => {
+    try {
+        dispatch({type: "LOADING"});
+        const result = await API.delete(`spots/${spotId}`);
+        dispatch({
+            type: "DELETE_SPOT",
+            payload: result.data
+        });
+    } catch (error) {
+        dispatch({ type: "ERROR", payload: error.response.data });
+    }
+}
+const deleteAllSpotsFromStation = async (stationId) => {
+    getSpotsByStation(stationId);
+    //comprobar el initial_state spotsByStation para poder borrarlos
+}
+
+export {
+    createSpot,
+    getAllSpots,
+    getSpotsByStation,
+    updateSpot,
+    deleteSpot,
+    deleteAllSpotsFromStation
+}
--- src/redux/spots/spots.reducer.js
+++ src/redux/spots/spots.reducer.js
@@ -1,7 +1,53 @@
 const INITIAL_STATE = {
-
+    spots: [],
+    spotsByStation: [],
+    loading: false,
+    error: null
 }
 
 export const spotsReducer = (state = INITIAL_STATE, action) => {
-    
+    switch (action.type) {
+        case "LOADING":
+            return {...state, loading: true};
+        case "CREATE_SPOT":
+            return {
+                ...state,
+                loading: false,
+                error: null
+            };
+        case "GET_SPOTS":
+            return {
+                ...state,
+                spots: [...action.payload],
+                loading: false,
+                error: null
+            };
+        case "STATION_SPOTS":
+            return {
+                ...state,
+                spotsByStation: [...action.payload],
+                loading: false,
+                error: null
+            };
+        case "UPDATE_SPOT":
+            return {
+                ...state,
+                loading: false,
+                error: null
+            }
+        case "DELETE_SPOT":
+            return {
+                ...state,
+                loading: false,
+                error: null
+            }
+        case "ERROR":
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
 }
