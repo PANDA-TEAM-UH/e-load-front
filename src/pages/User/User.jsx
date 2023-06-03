7a47a6aa66e062c45f11275b7153b923595ab978
Adrian <deariasadrian@gmail.com>
2023-06-03 13:22:23 +0200

    modified error in SideBar name component

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,11 +1,13 @@
 //import React from 'react'
+import SideBar from "../../components/SideBar/Sidebar"
 
 const User = () => {
   return (
     <div>
-      
+      <SideBar/>
     </div>
   )
 }
 
 export default User
+
