11d9a986c014a877810fa5f58df13fa6f880c4f1
Alojandro <aloserra92@gmail.com>
2023-06-05 20:04:25 +0200

    create home and components

---
+++ src/components/HomeSectionInstructions/HomeSectionInstructions.jsx
@@ -0,0 +1,46 @@
+// import React from 'react'
+
+const HomeSectionInstructions = () => {
+  return (
+    <section className="home__container-instructions">
+    <div className="home__header-instructions">
+<h3 className="title-instructions">Hazte de E-LOAD, la ciudad te espera.</h3>
+
+    </div>
+
+<div className="home__body-container">
+
+    <div className="home__body-instructions">
+    <img data-src="https://zity.eco/wp-content/uploads/2021/07/descargar-app.svg" alt="descargar-imagen" src="ruta"/>
+    <span className="number-instructions">1</span>
+<h4 className="body-instructions">Descarga la app</h4>
+
+    </div>
+
+    <div className="home__body-instructions">
+    <img data-src="https://zity.eco/wp-content/uploads/2021/07/descargar-app.svg" alt="registrate-imagen" src="ruta"/>
+    <span className="number-instructions">2</span>
+<h4 className="body-instructions">Registrate</h4>
+
+    </div>
+
+    <div className="home__body-instructions">
+    <img data-src="https://zity.eco/wp-content/uploads/2021/07/descargar-app.svg" alt="reserva-imagen" src="ruta"/>
+    <span className="number-instructions">3</span>
+<h4 className="body-instructions">Reserva tu estación</h4>
+
+    </div>
+
+    <div className="home__body-instructions">
+    <img data-src="https://zity.eco/wp-content/uploads/2021/07/descargar-app.svg" alt="coche-imagen" src="ruta"/>
+    <span className="number-instructions">4</span>
+<h4 className="body-instructions">Muévete!</h4>
+
+    </div>
+
+    </div>
+    </section>
+  )
+}
+
+export default HomeSectionInstructions
