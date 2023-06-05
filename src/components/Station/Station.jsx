69f42c8ef62a338c4b3cfd3305956a4cfabfb6db
fpascuales <fpascuales@gmail.com>
2023-06-06 00:16:17 +0200

    Add spots to station in Drawer component

--- src/components/Station/Station.jsx
+++ src/components/Station/Station.jsx
@@ -6,13 +6,14 @@ import {
   DrawerHeader,
   DrawerOverlay,
   DrawerContent,
-  DrawerCloseButton
+  DrawerCloseButton,
+  Card
 } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import { AccessTime, FavoriteBorder } from "@mui/icons-material";
 const Station = ({ isOpen, onClose }) => {
     const { stationSelected } = useSelector((state) => state.stations);
-    // const { spotsByStation } = useSelector((state) => state.spots);
+    const { spotsByStation } = useSelector((state) => state.spots);
   return (
     <Drawer
       isOpen={isOpen}
@@ -28,7 +29,16 @@ const Station = ({ isOpen, onClose }) => {
         <DrawerBody>
             <FavoriteBorder/>{stationSelected.likes}
             <AccessTime/>{stationSelected.schedule}
-
+            {spotsByStation.map((spot) => {
+              return (
+                <Card key={spot._id}>
+                  <p>Potencia {spot.power}</p>
+                  <p>Tipo {spot.type}</p>
+                  <p>Tarifa {spot.rate}</p>
+                  <p>Estado {spot.state}</p>
+                </Card>
+              )
+            })}
         </DrawerBody>
 
         <DrawerFooter></DrawerFooter>
