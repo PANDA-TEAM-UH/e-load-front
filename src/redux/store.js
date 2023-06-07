a5e3765ab78b9e8bbe278a1d5d519c3c46747a94
fpascuales <fpascuales@gmail.com>
2023-06-07 21:44:38 +0200

    Add paymentReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,14 @@ import { commentsReducer } from "./comments/comments.reducer";
 import { spotsReducer } from "./spots/spots.reducer";
 import { stationsReducer } from "./stations/stations.reducer";
 import { usersReducer } from "./users/users.reducer";
+import { paymentsReducer } from "./payments/payments.reducer";
 
 export default configureStore({
     reducer: {
         comments: commentsReducer,
         spots: spotsReducer,
         stations: stationsReducer,
-        users: usersReducer
+        users: usersReducer,
+        payments: paymentsReducer
     }
 })
