10df62208a2aa8927a1ae585f86d876e0abaa7eb
Alojandro <aloserra92@gmail.com>
2023-06-08 21:48:02 +0200

    create contact form

--- src/components/Routing/Routing.jsx
+++ src/components/Routing/Routing.jsx
@@ -1,18 +1,21 @@
 // import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import User from '../../pages/User/User'
+// import User from '../../pages/User/User'
 import Map from '../../pages/Map/Map'
 import PaymentContainer from '../PaymentContainer/PaymentContainer'
+import ContactForm from '../ContactForm/ContactForm'
 
 
 
 const Routing = () => {
   return (
     <Routes>
-      <Route path="/user" element={<User/>}/>
+      {/* <Route path="/user" element={<User/>}/>
+      
         {/* <Route path="/" element={<Inicio/>}></Route> */}
         <Route path="/" element={<Map/>}></Route>
         <Route path="/payment" element={<PaymentContainer />} />
+      <Route path="/contact" element={<ContactForm/>}/>
         {/* <Route path="/login" element={<SobreNosotros/>}></Route>
         <Route path="/login" element={<Contacto/>}></Route>
 
