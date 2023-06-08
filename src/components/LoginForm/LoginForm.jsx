2a2e3d68764c16e7ee3b8b8242cda6c7c5170e21
Adrian <deariasadrian@gmail.com>
2023-06-08 20:35:39 +0200

    added LoginForm, on submit close Modal && added chakra components

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,43 +1,49 @@
-// import { useNavigate } from 'react-router-dom';
-// import { Link, Navigate, useNavigate } from 'react-router-dom';
-// import {
-//     FormControl,
-//     FormLabel,
-//     Input,
-//     Button,
-//     FormErrorMessage,
-// } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import {
+    FormLabel,
+    Input,
+    Button,
+} from '@chakra-ui/react';
 import './LoginForm.scss';
 import { useForm } from 'react-hook-form';
 import { useSelector } from 'react-redux';
 import { login } from '../../redux/users/users.actions';
 
-const LoginForm = () => {
+const LoginForm = ({ onClose }) => {
     const { register, handleSubmit } = useForm();
     const { error } = useSelector((state) => state.users);
+    console.log(error);
+    function dataLogin(values) {
+        login(values)
+        if (error !== null) {
+            onClose();
+        }
+    }
+
     return (
-        <div className="b-login">
-            <form
-                className="b-login-form"
-                onSubmit={handleSubmit((dataLogin) => login(dataLogin))}
-            >
-                <input
-                    className="b-login-forminput"
-                    {...register("username")}
-                    placeholder="Usuario"
-                />
-                <input
-                    className="b-login-forminput"
-                    {...register("password")}
-                    placeholder="Contraseña"
-                    type="password"
-                />
-                <p className="b-login-formerror">{error}</p>
-                <button className="b-login-formbtn">
-                    Iniciar Sesión
-                </button>
-            </form>
-        </div>
+        <form
+            className="b-login-form"
+            onSubmit={handleSubmit(dataLogin)}
+        >
+            <FormLabel htmlFor="username">Usuario</FormLabel>
+            <Input
+                className="b-login-forminput"
+                {...register("username")}
+                placeholder="Usuario"
+            />
+            <FormLabel htmlFor="password">Contraseña</FormLabel>
+            <Input
+                className="b-login-forminput"
+                {...register("password")}
+                placeholder="Contraseña"
+                type="password"
+            />
+            <p className="b-login-formerror">{error}</p>
+            <Button className="b-login-formbtn" type="submit">
+                Iniciar Sesión
+            </Button>
+            <Link to="/register">No tienes cuenta? Regístrate</Link>
+        </form>
     );
 };
 
