76e283ecb375b63ab0563810fe9c08eaf88c0edc
Adrian <deariasadrian@gmail.com>
2023-06-08 23:11:22 +0200

    added /register in Routing

--- src/components/Routing/Routing.jsx
+++ src/components/Routing/Routing.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import User from '../../pages/User/User'
 import Map from '../../pages/Map/Map'
 import PaymentContainer from '../PaymentContainer/PaymentContainer'
-
+import Register from '../../components/Register/Register'
 
 
 const Routing = () => {
@@ -12,6 +12,7 @@ const Routing = () => {
       <Route path="/user" element={<User/>}/>
         {/* <Route path="/" element={<Inicio/>}></Route> */}
         <Route path="/" element={<Map/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
         <Route path="/payment" element={<PaymentContainer />} />
         {/* <Route path="/login" element={<SobreNosotros/>}></Route>
         <Route path="/login" element={<Contacto/>}></Route>
