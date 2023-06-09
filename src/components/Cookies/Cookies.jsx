eb60f3849a0959b6578296bc26069511761961d2
Alojandro <aloserra92@gmail.com>
2023-06-09 23:37:56 +0200

    update create cookies

--- src/components/Cookies/Cookies.jsx
+++ src/components/Cookies/Cookies.jsx
@@ -19,11 +19,9 @@ const Cookies = () => {
          <p>Nosotros y nuestros socios usamos cookies o tecnologías similares para garantizar el correcto funcionamiento del portal, recoger información sobre su uso, mejorar nuestros servicios y</p>
          <p> mostrarte publicidad personalizada basándonos en el análisis de tu navegación.</p>
          <p>Puedes configurar o rechazar en cualquier momento la utilización de cookies y también puedes retirar tu consentimiento u oponerte al tratamiento de tus datos que hacemos en base al interés</p>
-         <p>legítimo. Para obtener información sobre cómo hacerlo pincha aquí o visita nuestra política de privacidad.Si quieres saber quiénes son nuestros socios accede aquí.</p> 
+         <p>legítimo. Para obtener información sobre cómo hacerlo pincha aquí o visita nuestra política de privacidad.</p> 
 
-         <p>y nuestros socios hacemos el siguiente tratamiento de datos:</p>
-         <p>Almacenar o acceder a información en un dispositivo, Anuncios y contenido personalizados, medición de anuncios y del contenido, información sobre el público y desarrollo de productos, Datos de</p>
-         <p>localización geográfica precisa e identificación mediante las características de dispositivos</p>
+         
 
          <Button>Aceptar</Button>
          <Button>Cerrar</Button>
