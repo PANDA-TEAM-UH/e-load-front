37f84d5bb3f656f17ff0a096cae10b1f6f2bcfce
fpascuales <fpascuales@gmail.com>
2023-06-10 13:59:22 +0200

    Adrian's Beta tester

--- src/components/AdminStations/AdminStations.jsx
+++ src/components/AdminStations/AdminStations.jsx
@@ -1,7 +1,16 @@
-import { useEffect } from "react"
-import { getAllStationsAdmin } from "../../redux/stations/stations.actions"
-import { useSelector } from "react-redux"
-import { Button, ButtonGroup, Card, CardBody, CardFooter, CardHeader, Grid, GridItem } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { getAllStationsAdmin } from "../../redux/stations/stations.actions";
+import { useSelector } from "react-redux";
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Grid,
+  GridItem,
+} from "@chakra-ui/react";
 
 const AdminStations = () => {
   const { stations } = useSelector((state) => state.stations);
@@ -10,39 +19,31 @@ const AdminStations = () => {
   }, []);
   return (
     <div>
-    <h2>Estaciones</h2>
-    <Grid templateColumns='repeat(4, 1fr)' gap={6}>
-    {stations.map((station) => {
-      return (
-      <GridItem key={station._id} w='100%' h='40'>
-        <Card>
-          <CardHeader>
-            <h3>{station.address}</h3>
-          </CardHeader>
-          <CardBody>
-            
-          </CardBody>
-          <CardFooter>
-            <ButtonGroup>
-              <Button>
-                VER
-              </Button>
-              <Button variant='ghost' colorScheme='red'>
-                BORRAR
-              </Button>
-            </ButtonGroup>
-          </CardFooter>
-        </Card>
-              
-      </GridItem>
-
-      
-
-    )})
-    }
-    </Grid>
+      <h2>Estaciones</h2>
+      <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+        {stations.map((station) => {
+          return (
+            <GridItem key={station._id} w="100%" h="40">
+              <Card>
+                <CardHeader>
+                  <h3>{station.address}</h3>
+                </CardHeader>
+                <CardBody></CardBody>
+                <CardFooter>
+                  <ButtonGroup>
+                    <Button>VER</Button>
+                    <Button variant="ghost" colorScheme="red">
+                      BORRAR
+                    </Button>
+                  </ButtonGroup>
+                </CardFooter>
+              </Card>
+            </GridItem>
+          );
+        })}
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default AdminStations
+export default AdminStations;
