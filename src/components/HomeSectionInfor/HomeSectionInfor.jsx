b0c777f7d02fdaec129e4a0f1dfce9b2258048c2
Alojandro <aloserra92@gmail.com>
2023-06-11 13:36:20 +0200

    create styles home

--- src/components/HomeSectionInfor/HomeSectionInfor.jsx
+++ src/components/HomeSectionInfor/HomeSectionInfor.jsx
@@ -1,28 +1,28 @@
-import './HomeSectionInfor.scss'
+// import './HomeSectionInfor.scss'
 
-const HomeSectionInfor = () => {
-  return (
-    <section className="home__container-infor">
+// const HomeSectionInfor = () => {
+//   return (
+//     <section className="home__container-infor">
 
-      <div className="home__header-data">
-        <div className="home__header-data-numbers">
-          <p className="stations-register">150</p>
-          <p className="stations-text">Puntos de carga</p>
-        </div>
+//       <div className="home__header-data">
+//         <div className="home__header-data-numbers">
+//           <p className="stations-register">150</p>
+//           <p className="stations-text">Puntos de carga</p>
+//         </div>
 
-        <div className="home__header-data-numbers">
-          <p className="members-register">10000</p>
-          <p className="members-text">Miembros registrados</p>
-        </div>
+//         <div className="home__header-data-numbers">
+//           <p className="members-register">10000</p>
+//           <p className="members-text">Miembros registrados</p>
+//         </div>
 
-      </div>
+//       </div>
 
-      <div className="home__body-data">
-        <p className="">¿Quieres descubrir nuestros puntos de carga?</p>
-        {/* COMPONENTE BOTON QUE REDIRIJA AL MAPA */}
-      </div>
-    </section>
-  );
-}
+//       <div className="home__body-data">
+//         <p className="">¿Quieres descubrir nuestros puntos de carga?</p>
+//         {/* COMPONENTE BOTON QUE REDIRIJA AL MAPA */}
+//       </div>
+//     </section>
+//   );
+// }
 
-export default HomeSectionInfor
+// export default HomeSectionInfor
--- src/components/HomeSectionInstructions/HomeSectionInstructions.jsx
+++ src/components/HomeSectionInstructions/HomeSectionInstructions.jsx
@@ -1,46 +1,46 @@
-import './HomeSectionInstructions'
+// import './HomeSectionInstructions'
 
-const HomeSectionInstructions = () => {
-  return (
-    <section className="home__container-instructions">
-    <div className="home__header-instructions">
-<h3 className="title-instructions">Hazte de E-LOAD, la ciudad te espera.</h3>
+// const HomeSectionInstructions = () => {
+//   return (
+//     <section className="home__container-instructions">
+//     <div className="home__header-instructions">
+// <h3 className="title-instructions">Hazte de E-LOAD, la ciudad te espera.</h3>
 
-    </div>
+//     </div>
 
-<div className="home__body-container">
+// <div className="home__body-container">
 
-    <div className="home__body-instructions">
-    <img data-src="ICONO DE DESCARGAR" alt="descargar-imagen" src="ruta"/>
-    <span className="number-instructions">1</span>
-<h4 className="body-instructions">Descarga la app</h4>
+//     <div className="home__body-instructions">
+//     <img data-src="ICONO DE DESCARGAR" alt="descargar-imagen" src="ruta"/>
+//     <span className="number-instructions">1</span>
+// <h4 className="body-instructions">Descarga la app</h4>
 
-    </div>
+//     </div>
 
-    <div className="home__body-instructions">
-    <img data-src="ICONO INSTRUCCIONES" alt="registrate-imagen" src="ruta"/>
-    <span className="number-instructions">2</span>
-<h4 className="body-instructions">Registrate</h4>
+//     <div className="home__body-instructions">
+//     <img data-src="ICONO INSTRUCCIONES" alt="registrate-imagen" src="ruta"/>
+//     <span className="number-instructions">2</span>
+// <h4 className="body-instructions">Registrate</h4>
 
-    </div>
+//     </div>
 
-    <div className="home__body-instructions">
-    <img data-src="ICONO RESERVAR" alt="reserva-imagen" src="ruta"/>
-    <span className="number-instructions">3</span>
-<h4 className="body-instructions">Reserva tu estación</h4>
+//     <div className="home__body-instructions">
+//     <img data-src="ICONO RESERVAR" alt="reserva-imagen" src="ruta"/>
+//     <span className="number-instructions">3</span>
+// <h4 className="body-instructions">Reserva tu estación</h4>
 
-    </div>
+//     </div>
 
-    <div className="home__body-instructions">
-    <img data-src="ICONO DE UN COCHE" alt="coche-imagen" src="ruta"/>
-    <span className="number-instructions">4</span>
-<h4 className="body-instructions">Muévete!</h4>
+//     <div className="home__body-instructions">
+//     <img data-src="ICONO DE UN COCHE" alt="coche-imagen" src="ruta"/>
+//     <span className="number-instructions">4</span>
+// <h4 className="body-instructions">Muévete!</h4>
 
-    </div>
+//     </div>
 
-    </div>
-    </section>
-  )
-}
+//     </div>
+//     </section>
+//   )
+// }
 
-export default HomeSectionInstructions
+// export default HomeSectionInstructions
