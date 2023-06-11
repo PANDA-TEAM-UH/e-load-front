5d788863afbaa47b529446289d6b837373204c46
Adrian <deariasadrian@gmail.com>
2023-06-11 14:24:46 +0200

    added colors

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,14 @@ import store from './redux/store.js'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 const colors = {
-   
     defaultColor: '#010f20',
     secondaryColor: '#4be62e',
     whiteColor: '  #ffffff',
     blackColor: '#000000',
     grayColor: '#383838',
     lightColor: '#D3D3D3',
-  
+    redColor: '#eb2632',
+    orangeColor: '#F59A30'
 }
 
 const theme = extendTheme({colors})
