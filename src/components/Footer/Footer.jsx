c6557f8dd86ab8dc80653a39e1b6bc8c8dddd7f3
Alojandro <aloserra92@gmail.com>
2023-06-12 13:30:41 +0200

    upodate style footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,35 +16,31 @@ const Footer = () => {
     <Box
       bg={useColorModeValue("whiteColor")}
       color={useColorModeValue("blackColor")}
-      // py = {1}
     >
       <Container as={Stack} maxW={"6xl"} py={10}>
         <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}>
           <Stack align="flex-start">
             <Heading fontSize="2xl">Asistencia</Heading>
             <Text>
-               Lunes a jueves: 9:00 - 18:30 <br/>
-               Viernes: 9:00 - 15:00 <br/>
-               Fines de semana y festivos: <br/> Sin atención al cliente<br/>
-               Llamar: 91 078 07 11 <br/>
-               Correo electrónico: <br/>
-                contacto@e-load.es
+              Lunes a jueves: 9:00 - 18:3 <br/>0 Viernes: 9:00 - 15:00 Fines de semana
+              y festivos: Sin atención al cliente Llamar: 91 078 07 11 Correo
+              electrónico: contacto@e-load.es
             </Text>
           </Stack>
 
           <Stack align={"flex-start"}>
             <Heading fontSize="2xl">Compañia</Heading>
-            <Link href={"/sobre-nosotros"}>Sobre Nosotros</Link>
-            <Link href={"/contacto"}>Contacto</Link>
-            <Link href={"/mapa"}>Puntos de carga</Link>
+            <Link href={"#"}>Sobre Nosotros</Link>
+            <Link href={"#"}>Contacto</Link>
+            <Link href={"#"}>Puntos de carga</Link>
           </Stack>
 
           <Stack align={"flex-start"}>
             <Heading fontSize="2xl">Legal</Heading>
             <Link href={"#"}>Cookies</Link>
             <Link href={"#"}>Politica de privacidad</Link>
-            <Link href={"#"}>Terminos de servicio</Link>
-            
+            <Link href={"#"}>Terms of Service</Link>
+            <Link href={"#"}>Law Enforcement</Link>
           </Stack>
 
           <Stack align="flex-start">
@@ -53,13 +49,13 @@ const Footer = () => {
               alt="foto-movil"
               src="https://res.cloudinary.com/dgkm71mjf/image/upload/v1686482925/e-load/app-store_zfrjug.png"
               position="relative"
-              maxWidth="43%"
+              maxWidth="45%"
             />
             <Image
               alt="foto-movil"
               src="https://res.cloudinary.com/dgkm71mjf/image/upload/v1686482925/e-load/play-store_wwa4f0.png"
               position="relative"
-              maxWidth="43%"
+              maxWidth="45%"
             />
           </Stack>
         </SimpleGrid>
@@ -69,7 +65,6 @@ const Footer = () => {
         borderTopWidth={1}
         borderStyle={"solid"}
         borderColor={useColorModeValue("whiteColor", "blackColor")}
-        mb={6}
       >
         <Container
           as={Stack}
@@ -80,7 +75,7 @@ const Footer = () => {
           justify={{ md: "space-between" }}
           align={{ md: "center" }}
         >
-          <Text fontWeight="bold">© 2023 E - LOAD. ALL RIGHTS RESERVED</Text>
+          <Text>© 2023 E - LOAD. ALL RIGHTS RESERVED</Text>
 
           <Stack direction={"row"} spacing={8}>
             <Box>
