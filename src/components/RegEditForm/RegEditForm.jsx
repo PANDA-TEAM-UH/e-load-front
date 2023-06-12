2778b7a64a2baa011c122ed2ded659936ef64b68
Adrian <deariasadrian@gmail.com>
2023-06-12 10:53:20 +0200

    added avatar profile image, fixed flex items && added buttons styles && default image

--- src/components/RegEditForm/RegEditForm.jsx
+++ src/components/RegEditForm/RegEditForm.jsx
@@ -1,13 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-	FormControl,
-	FormLabel,
-	Input,
-	Button,
-	Flex,
-	Box,
-} from '@chakra-ui/react';
+import { FormLabel, Input, Button, Box, Avatar, Flex } from '@chakra-ui/react';
 import { updateUser } from '../../redux/users/users.actions';
 
 const RegEditForm = () => {
@@ -18,10 +11,12 @@ const RegEditForm = () => {
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		if (user) {
-			setSelectedImage(user.image || '');
-		}
-	}, [user]);
+		return () => {
+			if (selectedImage) {
+				URL.revokeObjectURL(selectedImage);
+			}
+		};
+	}, [selectedImage]);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -39,17 +34,25 @@ const RegEditForm = () => {
 	};
 
 	return (
-		<div className="reg-edit-form">
-			<Flex>
-				<Box w="200px" mr="4">
-					{selectedImage && <img src={selectedImage} alt="User" />}
+		<Box>
+			<Flex as={Flex} flexWrap="wrap">	
+				<Box w="200px" pr="4" style={{ margin: '0 auto' }} align="center">
+					<Box mb={4}>
+						{selectedImage ? (
+							<Avatar size='xl' src={selectedImage} />
+						) : (
+							<Avatar size='xl' src="https://res.cloudinary.com/dgkm71mjf/image/upload/v1686469069/e-load/e-load-avatar_e0k9w4.png" alt="User" />
+						)}
+					</Box>
 					<Button
 						as="label"
 						htmlFor="image-upload"
 						variant="outline"
-						colorScheme="blue"
+						colorScheme="defaultColor"
 						mt={2}
 						cursor="pointer"
+						_hover={{bg: "defaultColor", color:"whiteColor"}}
+						mb={5}
 					>
 						Editar imagen
 					</Button>
@@ -64,48 +67,46 @@ const RegEditForm = () => {
 					/>
 				</Box>
 				<form onSubmit={handleSubmit}>
-					<FormControl>
-						<FormLabel>Nombre de usuario</FormLabel>
-						<Input
-							type="text"
-							name="username"
-							defaultValue={user?.username || ''}
-						/>
+					<FormLabel fontWeight={600}>Nombre de usuario</FormLabel>
+					<Input
+						type="text"
+						name="username"
+						defaultValue={user?.username || ''}
+					/>
 
-						<FormLabel>Nombre</FormLabel>
-						<Input
-							type="text"
-							name="name"
-							defaultValue={user?.name || ''}
-						/>
+					<FormLabel fontWeight={600} pt={2}>Nombre</FormLabel>
+					<Input
+						type="text"
+						name="name"
+						defaultValue={user?.name || ''}
+					/>
 
-						<FormLabel>Apellidos</FormLabel>
-						<Input
-							type="text"
-							name="surnames"
-							defaultValue={user?.surnames || ''}
-						/>
+					<FormLabel fontWeight={600} pt={2}>Apellidos</FormLabel>
+					<Input
+						type="text"
+						name="surnames"
+						defaultValue={user?.surnames || ''}
+					/>
 
-						<FormLabel>Email</FormLabel>
-						<Input
-							type="email"
-							name="email"
-							defaultValue={user?.email || ''}
-						/>
+					<FormLabel fontWeight={600} pt={2}>Email</FormLabel>
+					<Input
+						type="email"
+						name="email"
+						defaultValue={user?.email || ''}
+					/>
 
-						<FormLabel>Contraseña</FormLabel>
-						<Input
-							type="password"
-							name="password"
-							defaultValue={user?.password || ''}
-						/>
-						<Button type="submit" colorScheme="blue" mt={4}>
-							{user ? 'Guardar' : 'Registrarse'}
-						</Button>
-					</FormControl>
+					<FormLabel fontWeight={600} pt={2}>Contraseña</FormLabel>
+					<Input
+						type="password"
+						name="password"
+						defaultValue={user?.password || ''}
+					/>
+					<Button type="submit" bg={'defaultColor'} color={'whiteColor'} _hover={{bg: "secondaryColor", color:"defaultColor"}} mt={4}>
+						Guardar
+					</Button>
 				</form>
 			</Flex>
-		</div>
+		</Box>
 	);
 };
 
