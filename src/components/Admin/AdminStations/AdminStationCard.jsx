5ad76a1515404db15d35384e150561ce27e8468c
fpascuales <fpascuales@gmail.com>
2023-06-12 04:41:52 +0200

    Add deleteStation on Admin Panel

--- src/components/Admin/AdminStations/AdminStationCard.jsx
+++ src/components/Admin/AdminStations/AdminStationCard.jsx
@@ -1,51 +1,53 @@
 import PropTypes from "prop-types";
-import {
-  Button,
-  ButtonGroup,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Divider,
-  GridItem,
-  Image,
-} from "@chakra-ui/react";
+import { Button, ButtonGroup, Card, CardBody, CardFooter, CardHeader, Divider, GridItem, Image, useDisclosure } from "@chakra-ui/react";
 import { Link, NavLink } from "react-router-dom";
+import AdminStationDeleteAlert from "./AdminStationDeleteAlert";
 
 const AdminStationCard = ({ station }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <GridItem key={station._id} w="100%">
-      <Card minH={'350px'}>
-        <CardHeader margin={0} padding={0}>
-          <Image src="https://res.cloudinary.com/dgkm71mjf/image/upload/v1686470412/e-load/e-load-station-02_xqi2zr.png" />
-          <Divider />
-        </CardHeader>
-        <CardBody>
-          <h3>{station.address}</h3>
-        </CardBody>
-        <CardFooter>
-          <ButtonGroup>
-            <Link
-              as={NavLink}
-              to={`/usuario/estaciones-detalle/${station._id}`}
-            >
+    <>
+      <GridItem key={station._id} w="100%">
+        <Card minH={"350px"}>
+          <CardHeader margin={0} padding={0}>
+            <Image src="https://res.cloudinary.com/dgkm71mjf/image/upload/v1686470412/e-load/e-load-station-02_xqi2zr.png" />
+            <Divider />
+          </CardHeader>
+          <CardBody>
+            <h3>{station.address}</h3>
+          </CardBody>
+          <CardFooter>
+            <ButtonGroup>
+              <Link
+                as={NavLink}
+                to={`/usuario/estaciones-detalle/${station._id}`}
+              >
+                <Button
+                  bg={"lightColor"}
+                  color={"defaultColor"}
+                  _hover={{ bg: "secondaryColor", color: "defaultColor" }}
+                >
+                  VER
+                </Button>
+              </Link>
               <Button
-                bg={'lightColor'} color={'defaultColor'} _hover={{bg: "secondaryColor", color:"defaultColor"}}
-              >VER</Button>
-            </Link>
-            <Button
-              variant="ghost"
-              color={'redColor'} _hover={{bg: "redColor", color:"whiteColor"}}
-            >
-              BORRAR
-            </Button>
-          </ButtonGroup>
-        </CardFooter>
-      </Card>
-    </GridItem>
+                variant="ghost"
+                color={"redColor"}
+                _hover={{ bg: "redColor", color: "whiteColor" }}
+                onClick={onOpen}
+              >
+                BORRAR
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Card>
+      </GridItem>
+      {isOpen && <AdminStationDeleteAlert isOpen={isOpen} onClose={onClose} station={station} isAdminStationDetail/>}
+    </>
   );
 };
 AdminStationCard.propTypes = {
-  station: PropTypes.object,
+  station: PropTypes.object
 };
 export default AdminStationCard;
--- src/components/Admin/AdminStations/AdminStations.jsx
+++ src/components/Admin/AdminStations/AdminStations.jsx
@@ -2,16 +2,17 @@ import { useEffect } from "react";
 import { getAllStationsAdmin } from "../../../redux/stations/stations.actions";
 import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
-import { Button, Divider, Flex, Grid, Spacer } from "@chakra-ui/react";
 import AdminStationCard from "./AdminStationCard";
 import { Link, NavLink } from "react-router-dom";
+import { Button, Divider, Flex, Grid, Spacer } from "@chakra-ui/react";
 
 const AdminStations = () => {
   const { stationsAdmin } = useSelector((state) => state.stations);
   useEffect(() => {
     getAllStationsAdmin();
-  }, []);
-  return (
+  }, [stationsAdmin]);
+
+   return (
     <div>
       <Flex>
         <h2>Estaciones</h2>
@@ -26,7 +27,7 @@ const AdminStations = () => {
       <Divider my={5} />
       <Grid templateColumns="repeat(6, 1fr)" gap={6} gridAutoFlow="row dense">
         {stationsAdmin.map((station) => {
-          return <AdminStationCard station={station} key={station._id} />;
+          return <AdminStationCard station={station} key={station._id}/>;
         })}
       </Grid>
     </div>
